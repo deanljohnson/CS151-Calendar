747bc374c1826798741799ef620e51ce4a5e2f09
deanljohnson <dean@deanljohnson.com>
2016-07-21 21:37:54 -0700

    Added some skeleton code to handle changing the event view

--- Calendar/src/CalendarWithEvents.java
+++ Calendar/src/CalendarWithEvents.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 public class CalendarWithEvents extends GregorianCalendar {
+	private ArrayList<ChangeListener> changeListeners = new ArrayList<ChangeListener>();
+	
 	public CalendarWithEvents(){	
 		//TODO: Setup the events map and initialize the calendar	
+	}
+	
+	public void addChangeListener(ChangeListener l){
+		changeListeners.add(l);
+	}
+	
+	@Override
+	public void set(int field, int value){
+		super.set(field, value);
+		notifyOfChange();
+	}
+	
+	private void notifyOfChange(){
+		ChangeEvent ce = new ChangeEvent(this);
+		for (ChangeListener l : changeListeners)
+			l.stateChanged(ce);
 	}
 }
--- Calendar/src/EventDisplayPanel.java
+++ Calendar/src/EventDisplayPanel.java
@@ -3,6 +3,8 @@ import java.awt.Dimension;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class EventDisplayPanel extends JPanel {
 	public enum FilterType{
@@ -10,12 +12,18 @@ public class EventDisplayPanel extends JPanel {
 	}
 	
 	private CalendarWithEvents calendar;
+	private FilterType filter = FilterType.Day;
 	private int width;
 	private int height;
 	
 	public EventDisplayPanel(CalendarWithEvents cal, int w, int h){
 		calendar = cal;
 		
+		calendar.addChangeListener(new ChangeListener(){
+			public void stateChanged(ChangeEvent e) {
+				refreshView();
+			}
+		});
 		//We want to track the width and height - we will use this info
 		//to create the icons for each individual day later
 		//when we are viewing events by week/month
@@ -31,6 +39,11 @@ public class EventDisplayPanel extends JPanel {
 	}
 	
 	public void setFilterType(FilterType t){
+		filter = t;
+		refreshView();
+	}
+	
+	private void refreshView(){
 		//In here we will create and change the view to display
 		//the events from a particular day/week/month or to show
 		//the agenda view
